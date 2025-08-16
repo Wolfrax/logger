744bc037dcdb72846e4e87a8da06eb89911d4be5
mm <mats.melander@gmail.com>
2025-08-16 19:59:32 +0200

    Purge entries from logger.json file that is older than 4 weeks,

--- logd.py
+++ logd.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, abort
 import json
-
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 FN = "/home/pi/app/logger/logger.json"
@@ -28,17 +28,38 @@ def root():
 @app.route('/log', methods=['GET', 'POST', 'DELETE'])
 def log():
     if request.method == 'POST':
-        log = {'msg': request.form['msg'],
-               'pathname': request.form['pathname'],
-               'created': request.form['created'],
-               'levelname': request.form['levelname']}
-        DB['data'].insert(0, log)
+        log_entry = {
+            'msg': request.form['msg'],
+            'pathname': request.form['pathname'],
+            'created': request.form['created'],
+            'levelname': request.form['levelname']
+        }
+
+        # Insert new entry at the top
+        DB['data'].insert(0, log_entry)
+
+        # Parse new entry's timestamp
+        try:
+            new_created = datetime.fromtimestamp(float(log_entry['created']))
+        except Exception:
+            # If created is not a timestamp, skip purge
+            new_created = None
+
+        # Purge old entries (older than 4 weeks relative to new entry)
+        if new_created:
+            cutoff = new_created - timedelta(weeks=4)
+            DB['data'] = [
+                entry for entry in DB['data']
+                if datetime.fromtimestamp(float(entry['created'])) >= cutoff
+            ]
 
         with open(FN, "w") as f:
             json.dump(DB, f, indent=4)
         return ""
+
     elif request.method == 'GET':
         return DB
+
     elif request.method == 'DELETE':
         data = dict(request.form)
         if data in DB['data']:
@@ -52,6 +73,7 @@ def log():
         abort(400)
 
 
+# Load existing DB
 try:
     with open(FN, "r") as f:
         DB = json.load(f)
@@ -59,3 +81,4 @@ except json.decoder.JSONDecodeError:
     pass  # File exists but is empty
 except FileNotFoundError:
     pass
+
